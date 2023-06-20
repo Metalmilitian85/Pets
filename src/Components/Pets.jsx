b8bc1c506dc8b6c4e03d2f875a801d3dcbce2b79
Tim <timward1985@gmail.com>
2023-06-20 09:32:53 -0500

    Pics2

--- src/Components/Pets.jsx
+++ src/Components/Pets.jsx
@@ -5,28 +5,28 @@ export default function Pets(props) {
     return (
       <div className="flex flex-wrap justify-center">
         <div class="max-w-sm rounded overflow-hidden shadow-lg m-8">
-            <Info img="../public/Skarin.jpg" />
+            <Info img="/Skarin.jpg" />
             <Info name="Skarin" />
             <Info type="Cat" />
             <Info age="12" />
             <Info bio="'Tubby kitty at heart'" />
         </div>
         <div class="max-w-sm rounded overflow-hidden shadow-lg m-8">
-            <Info img="../public/Tiger.jpg" />
+            <Info img="/Tiger.jpg" />
             <Info name="Tiger" />
             <Info type="Cat" />
             <Info age="12" />
             <Info bio="'Killer kittay'" />
         </div>
         <div class="max-w-sm rounded overflow-hidden shadow-lg m-8">
-            <Info img="../public/Tripp.jpg" />
+            <Info img="/Tripp.jpg" />
             <Info name="Tripp" />
             <Info type="Dog" />
             <Info age="8" />
             <Info bio="'Pee attack'" />
         </div>
         <div class="max-w-sm rounded overflow-hidden shadow-lg m-8">
-            <Info img="../public/Harley.jpg" />
+            <Info img="/Harley.jpg" />
             <Info name="Harley" />
             <Info type="Dog" />
             <Info age="7" />
